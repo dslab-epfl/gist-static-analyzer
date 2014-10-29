0fbf06797d0aee1550cab29e25615a5c0b740fdf
Baris Kasikci <bariskasikci@gmail.com>
2014-10-29 01:14:22 +0100

    checkpoint

--- lib/Transforms/DebugInfoLocator/DebugInfoLocator.cpp
+++ lib/Transforms/DebugInfoLocator/DebugInfoLocator.cpp
@@ -8,46 +8,60 @@
 #include <llvm/IR/Constants.h>
 #include <llvm/IR/InlineAsm.h>
 
+#include "llvm/IR/DebugInfo.h"
 #include "DebugInfoLocator.h"
 
 using namespace llvm;
 using namespace std;
 
-namespace llvm {
-  cl::opt<string>
-  FileName("file-name",
-	    cl::desc("The file name that contains the target instruction"),
-	    cl::init(""));
-  cl::opt<string>
-  FunctionName("function-name",
-	       cl::desc("The function where the instruction lies"),
-	       cl::init(""));
-  cl::opt<string>
-  LineNumber("line-number",
-	     cl::desc("the line number of the target instruction"),
-	     cl::init(""));
+static  cl::opt<string> FileName("file-name",
+                                 cl::desc("The file name that contains the target instruction"),
+                                 cl::init(""));
+static  cl::opt<string> FunctionName("function-name",
+                                     cl::desc("The function where the instruction lies"),
+                                     cl::init(""));
+static  cl::opt<string> LineNumber("line-number",
+                                   cl::desc("the line number of the target instruction"),
+                                   cl::init(""));
+
+DebugInfoLocator::DebugInfoLocator() : ModulePass(ID){
+  if (strlen(FileName.ValueStr) == 0 )
+    errs() << "No no" << "\n";
+  //errs() << FileName;
+  
 }
 
+// TODO: we should cache the results once they are computed for a given binary.
 bool DebugInfoLocator::runOnModule(Module& m) {
   for (Module::iterator fi = m.begin(), fe = m.end(); fi != fe; ++fi) {
     errs().write_escaped(fi->getName()) << "\n";
     for (Function::iterator bi = fi->begin(), be = fi->end(); bi != be; ++bi) {
-      //             !fi->getName().equals(StringRef("_Z13resMarkerFuncv")))
-      for (BasicBlock::iterator ii = bi->begin(), ie = bi->end(); ii != ie; ++ii) {
-	;
-      }
-    }
-  }
+      if(FunctionName.hasArgStr() && fi->getName().find(StringRef(FunctionName)) != StringRef::npos)
+        for (BasicBlock::iterator ii = bi->begin(), ie = bi->end(); ii != ie; ++ii) { 
+          if (MDNode *N = ii->getMetadata("dbg")) {
+            DILocation Loc(N);
+            unsigned Line = Loc.getLineNumber();
+            StringRef File = Loc.getFilename();
+            StringRef Dir = Loc.getDirectory();
+      	    errs() << Dir << "/" << File.str() << " : " << Line << *ii << "\n ";
+          }
+        }
+     }
+   }
   return true;
+}
+                                                                                                                          
+void DebugInfoLocator::getAnalysisUsage(AnalysisUsage &au) const {
+  // We don't modify the program and preserve all the passes
+  au.setPreservesAll();
+}
+
+const char* DebugInfoLocator::getPassName() const { 
+  return "DebugInfoLocator"; 
 }
 
 char DebugInfoLocator::ID = 0;
 
-static void registerMyPass(const PassManagerBuilder &,
-                           PassManagerBase &PM) {
-    PM.add(new DebugInfoLocator());
-}
-
-static RegisterStandardPasses
-    RegisterMyPass(PassManagerBuilder::EP_EnabledOnOptLevel0,
-                   registerMyPass);
+static RegisterPass<DebugInfoLocator> X("debug-info-locator", "Debug information locator",
+                             true /* Only looks at CFG */,
+                             true /* Analysis Pass */);
--- lib/Transforms/DebugInfoLocator/DebugInfoLocator.h
+++ lib/Transforms/DebugInfoLocator/DebugInfoLocator.h
@@ -3,11 +3,11 @@
 namespace llvm{
   class DebugInfoLocator : public ModulePass {
   public:
-    DebugInfoLocator() : ModulePass(ID){}
+    DebugInfoLocator();
 
-    const char *getPassName() const override { return "DebugInfoLocator"; }
+    const char *getPassName() const override;
     virtual bool runOnModule(Module& m);
-
+    virtual void getAnalysisUsage(AnalysisUsage& au) const override;
     static char ID;
   };
 }
