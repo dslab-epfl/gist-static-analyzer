7e78e7908d42bab07cde81f8693764b52f0e6340
Benjamin Schubert <benjamin.schubert@epfl.ch>
2015-03-24 23:11:28 +0100

    sync up

--- lib/Transforms/IntelPTInstrumentor/IntelPTInstrumentor.cpp
+++ lib/Transforms/IntelPTInstrumentor/IntelPTInstrumentor.cpp
@@ -30,157 +30,157 @@
 using namespace llvm;
 using namespace std;
 
-static cl::opt<string> StartFileName("start-file-name",
-       cl::desc("File name to start PT tracing"),
-       cl::init(""));
-static cl::opt<string> StartFunction("start-function",
-       cl::desc("Function to Start PT tracing"),
-       cl::init(""));
-static cl::opt<unsigned> StartLineNumber("start-line-number",
-       cl::desc("Line number to start PT tracing"),
-       cl::init(0));
-static cl::opt<unsigned> StartIndex("start-index",
-       cl::desc("Index of the starting instruction with the matching file name and line number"),
-       cl::init(0));
+// static cl::opt<string> StartFileName("start-file-name",
+//        cl::desc("File name to start PT tracing"),
+//        cl::init(""));
+// static cl::opt<string> StartFunction("start-function",
+//        cl::desc("Function to Start PT tracing"),
+//        cl::init(""));
+// static cl::opt<unsigned> StartLineNumber("start-line-number",
+//        cl::desc("Line number to start PT tracing"),
+//        cl::init(0));
+// static cl::opt<unsigned> StartIndex("start-index",
+//        cl::desc("Index of the starting instruction with the matching file name and line number"),
+//        cl::init(0));
 
-static cl::opt<string> StopFileName("stop-file-name",
-       cl::desc("File name to stop PT tracing"),
-       cl::init(""));
-static cl::opt<string> StopFunction("stop-function",
-       cl::desc("Function to Stop PT tracing"),
-       cl::init(""));
-static cl::opt<unsigned> StopLineNumber("stop-line-number",
-       cl::desc("Line number to stop PT tracing"),
-       cl::init(0));
-static cl::opt<unsigned> StopIndex("stop-index",
-       cl::desc("Index of the stopping instruction with the matching file name and line number"),
-       cl::init(0));
-
-
-IntelPTInstrumentor::IntelPTInstrumentor() : ModulePass(ID) {
-  if (StartFileName == "" || StartLineNumber == 0 || StartIndex == 0 ||
-      StopFileName == "" || StopLineNumber == 0 || StopLineNumber == 0) {
-    errs() << "\nYou need to provide the start/stop file name, function name, and "
-           << "the line number!!!" << "\n\n";
-              exit(1);
-  }
-}
+// static cl::opt<string> StopFileName("stop-file-name",
+//        cl::desc("File name to stop PT tracing"),
+//        cl::init(""));
+// static cl::opt<string> StopFunction("stop-function",
+//        cl::desc("Function to Stop PT tracing"),
+//        cl::init(""));
+// static cl::opt<unsigned> StopLineNumber("stop-line-number",
+//        cl::desc("Line number to stop PT tracing"),
+//        cl::init(0));
+// static cl::opt<unsigned> StopIndex("stop-index",
+//        cl::desc("Index of the stopping instruction with the matching file name and line number"),
+//        cl::init(0));
 
 
-const char* IntelPTInstrumentor::getPassName() const {
-  return "DebugInfoLocator";
-}
+// IntelPTInstrumentor::IntelPTInstrumentor() : ModulePass(ID) {
+//   if (StartFileName == "" || StartLineNumber == 0 || StartIndex == 0 ||
+//       StopFileName == "" || StopLineNumber == 0 || StopLineNumber == 0) {
+//     errs() << "\nYou need to provide the start/stop file name, function name, and "
+//            << "the line number!!!" << "\n\n";
+//               exit(1);
+//   }
+// }
 
 
-void IntelPTInstrumentor::getAnalysisUsage(AnalysisUsage &au) const {
-  // We don't modify the program and preserve all the passes
-  au.setPreservesAll();
-}
+// const char* IntelPTInstrumentor::getPassName() const {
+//   return "DebugInfoLocator";
+// }
 
 
-void IntelPTInstrumentor::printDebugInfo(Instruction& instr) {
-  MDNode *N = instr.getMetadata("dbg");
-  DILocation Loc(N);
-  unsigned lineNumber = Loc.getLineNumber();
-  StringRef fileName = Loc.getFilename();
-  StringRef directory = Loc.getDirectory();
-  errs() << "\t\t" << directory << "/" << fileName<< " : " << lineNumber << "\n ";
-}
-
-set<string>& IntelPTInstrumentor::split(const string &s, char delim, 
-                                        set<string> &elems) {
-  stringstream ss(s);
-  string item;
-  while (getline(ss, item, delim)) {
-    elems.insert(item);
-  }
-  return elems;
-}
+// void IntelPTInstrumentor::getAnalysisUsage(AnalysisUsage &au) const {
+//   // We don't modify the program and preserve all the passes
+//   au.setPreservesAll();
+// }
 
 
-set<string> IntelPTInstrumentor::split(const string &s, char delim) {
-  set<string> elems;
-  split(s, delim, elems);
-  return elems;
-}
+// void IntelPTInstrumentor::printDebugInfo(Instruction& instr) {
+//   MDNode *N = instr.getMetadata("dbg");
+//   DILocation Loc(N);
+//   unsigned lineNumber = Loc.getLineNumber();
+//   StringRef fileName = Loc.getFilename();
+//   StringRef directory = Loc.getDirectory();
+//   errs() << "\t\t" << directory << "/" << fileName<< " : " << lineNumber << "\n ";
+// }
+
+// set<string>& IntelPTInstrumentor::split(const string &s, char delim, 
+//                                         set<string> &elems) {
+//   stringstream ss(s);
+//   string item;
+//   while (getline(ss, item, delim)) {
+//     elems.insert(item);
+//   }
+//   return elems;
+// }
 
 
-bool IntelPTInstrumentor::runOnModule(Module& m) {
-
-  // Type Definitions                                                                                                                  
-  std::vector<Type*>FuncTy_0_args;
-  FunctionType* FuncTy_0 = FunctionType::get(
-                                             /*Result=*/Type::getVoidTy(m.getContext()),
-                                             /*Params=*/FuncTy_0_args,
-                                             /*isVarArg=*/false);
+// set<string> IntelPTInstrumentor::split(const string &s, char delim) {
+//   set<string> elems;
+//   split(s, delim, elems);
+//   return elems;
+// }
 
 
-  // Function Declarations                                                                                                             
+// bool IntelPTInstrumentor::runOnModule(Module& m) {
 
-  Function* func_startPt = m.getFunction("startPt");
-  if (!func_startPt) {
-    func_startPt = Function::Create(
-                                    /*Type=*/FuncTy_0,
-                                    /*Linkage=*/GlobalValue::ExternalLinkage,
-                                    /*Name=*/"startPt", &m);
-    func_startPt->setCallingConv(CallingConv::C);
-  }
+//   // Type Definitions                                                                                                                  
+//   std::vector<Type*>FuncTy_0_args;
+//   FunctionType* FuncTy_0 = FunctionType::get(
+//                                              /*Result=*/Type::getVoidTy(m.getContext()),
+//                                              /*Params=*/FuncTy_0_args,
+//                                              /*isVarArg=*/false);
 
-  Function* func_stopPt = m.getFunction("stopPt");
-  if (!func_stopPt) {
-    func_stopPt = Function::Create(
-                                   /*Type=*/FuncTy_0,
-                                   /*Linkage=*/GlobalValue::ExternalLinkage,
-                                   /*Name=*/"stopPt", &m);
-    func_stopPt->setCallingConv(CallingConv::C);
-  }
 
-  unsigned startIndex = 1;
-  unsigned stopIndex = 1;
+//   // Function Declarations                                                                                                             
 
-  for (Module::iterator fi = m.begin(), fe = m.end(); fi != fe; ++fi) {    
-    for (Function::iterator bi = fi->begin(), be = fi->end(); bi != be; ++bi) {
-      if(fi->getName().find(StringRef(StartFunction)) != StringRef::npos) {
-        for (BasicBlock::iterator ii = bi->begin(), ie = bi->end(); ii != ie; ++ii) { 
-          if (MDNode *N = ii->getMetadata("dbg")) {
-            DILocation Loc(N);
-            unsigned lineNumber = Loc.getLineNumber();
-            if(lineNumber == StartLineNumber) {
-              StringRef fileName = Loc.getFilename();
-              if ((fileName.find(StringRef(StartFileName))) != StringRef::npos) {
-                if(startIndex == StartIndex) {
-                  startIndex = 1;
-                  CallInst::Create(func_startPt, "startPT", ii);
-                }
-              }
-            }             
-          }
-        }
-      }
-      if(fi->getName().find(StringRef(StopFunction)) != StringRef::npos) {
-        for (BasicBlock::iterator ii = bi->begin(), ie = bi->end(); ii != ie; ++ii) {
-          if (MDNode *N = ii->getMetadata("dbg")) {
-            DILocation Loc(N);
-            unsigned lineNumber = Loc.getLineNumber();            
-            if(lineNumber == StopLineNumber) {
-              StringRef fileName = Loc.getFilename();
-              if ((fileName.find(StringRef(StopFileName))) != StringRef::npos) {
-                if(stopIndex == StopIndex) {
-                  stopIndex = 1;
-                  CallInst::Create(func_stopPt, "stopPT", ii);
-                }            
-              }              
-            }
-          }
-        }
-      }      
-    }
-  }
-  return false;
-}
+//   Function* func_startPt = m.getFunction("startPt");
+//   if (!func_startPt) {
+//     func_startPt = Function::Create(
+//                                     /*Type=*/FuncTy_0,
+//                                     /*Linkage=*/GlobalValue::ExternalLinkage,
+//                                     /*Name=*/"startPt", &m);
+//     func_startPt->setCallingConv(CallingConv::C);
+//   }
 
-char IntelPTInstrumentor::ID = 0;
+//   Function* func_stopPt = m.getFunction("stopPt");
+//   if (!func_stopPt) {
+//     func_stopPt = Function::Create(
+//                                    /*Type=*/FuncTy_0,
+//                                    /*Linkage=*/GlobalValue::ExternalLinkage,
+//                                    /*Name=*/"stopPt", &m);
+//     func_stopPt->setCallingConv(CallingConv::C);
+//   }
 
-static RegisterPass<IntelPTInstrumentor> X("intel-pt-instrumentor", "Intel PT Instrumentor",
-                                         true /* Only looks at CFG */,
-                                         true /* Analysis Pass */);
+//   unsigned startIndex = 1;
+//   unsigned stopIndex = 1;
+
+//   for (Module::iterator fi = m.begin(), fe = m.end(); fi != fe; ++fi) {    
+//     for (Function::iterator bi = fi->begin(), be = fi->end(); bi != be; ++bi) {
+//       if(fi->getName().find(StringRef(StartFunction)) != StringRef::npos) {
+//         for (BasicBlock::iterator ii = bi->begin(), ie = bi->end(); ii != ie; ++ii) { 
+//           if (MDNode *N = ii->getMetadata("dbg")) {
+//             DILocation Loc(N);
+//             unsigned lineNumber = Loc.getLineNumber();
+//             if(lineNumber == StartLineNumber) {
+//               StringRef fileName = Loc.getFilename();
+//               if ((fileName.find(StringRef(StartFileName))) != StringRef::npos) {
+//                 if(startIndex == StartIndex) {
+//                   startIndex = 1;
+//                   CallInst::Create(func_startPt, "startPT", ii);
+//                 }
+//               }
+//             }             
+//           }
+//         }
+//       }
+//       if(fi->getName().find(StringRef(StopFunction)) != StringRef::npos) {
+//         for (BasicBlock::iterator ii = bi->begin(), ie = bi->end(); ii != ie; ++ii) {
+//           if (MDNode *N = ii->getMetadata("dbg")) {
+//             DILocation Loc(N);
+//             unsigned lineNumber = Loc.getLineNumber();            
+//             if(lineNumber == StopLineNumber) {
+//               StringRef fileName = Loc.getFilename();
+//               if ((fileName.find(StringRef(StopFileName))) != StringRef::npos) {
+//                 if(stopIndex == StopIndex) {
+//                   stopIndex = 1;
+//                   CallInst::Create(func_stopPt, "stopPT", ii);
+//                 }            
+//               }              
+//             }
+//           }
+//         }
+//       }      
+//     }
+//   }
+//   return false;
+// }
+
+// char IntelPTInstrumentor::ID = 0;
+
+// static RegisterPass<IntelPTInstrumentor> X("intel-pt-instrumentor", "Intel PT Instrumentor",
+//                                          true /* Only looks at CFG */,
+//                                          true /* Analysis Pass */);
--- lib/Transforms/IntelPTInstrumentor/IntelPTInstrumentor.h
+++ lib/Transforms/IntelPTInstrumentor/IntelPTInstrumentor.h
@@ -1,22 +1,22 @@
-#include<set>
+/* #include<set> */
 
-namespace llvm{
-  class IntelPTInstrumentor : public ModulePass {
-  public:
-    IntelPTInstrumentor();
+/* namespace llvm{ */
+/*   class IntelPTInstrumentor : public ModulePass { */
+/*   public: */
+/*     IntelPTInstrumentor(); */
 
-    const char *getPassName() const;
-    virtual void getAnalysisUsage(AnalysisUsage& au) const;
-    void printDebugInfo(Instruction& instr);
+/*     const char *getPassName() const; */
+/*     virtual void getAnalysisUsage(AnalysisUsage& au) const; */
+/*     void printDebugInfo(Instruction& instr); */
 
-    virtual bool runOnModule(Module& m);
-    std::vector<Value*> targetOperands;
-    std::vector<Instruction*> targetInstructions;
-    std::vector<Function*> targetFunctions;
+/*     virtual bool runOnModule(Module& m); */
+/*     std::vector<Value*> targetOperands; */
+/*     std::vector<Instruction*> targetInstructions; */
+/*     std::vector<Function*> targetFunctions; */
 
-    std::set<std::string>& split(const std::string &s, char delim, std::set<std::string> &elems);
-    std::set<std::string> split(const std::string &s, char delim);
+/*     std::set<std::string>& split(const std::string &s, char delim, std::set<std::string> &elems); */
+/*     std::set<std::string> split(const std::string &s, char delim); */
 
-    static char ID;
-  };
-}
+/*     static char ID; */
+/*   }; */
+/* } */
